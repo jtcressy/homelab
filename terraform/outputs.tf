c0d16803ba946f7a37c7524bfe13c45c8f6236b2
Joel Cressy <joel@jtcressy.net>
2021-08-08 09:31:34 +0000

    condense outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -18,21 +18,15 @@ output "zerotier_identities" {
       id = zerotier_identity.home.id
       private = zerotier_identity.home.private_key
       public = zerotier_identity.home.public_key
+      setup_command = <<-EOT
+        mkdir -p /mnt/data/zerotier-one && \
+        echo "${zerotier_network.jtcressy_net.id}=eth12" > /mnt/data/zerotier-one/devicemap && \
+        echo "${zerotier_identity.home.public_key}" > /mnt/data/zerotier-one/identity.public && \
+        echo "${zerotier_identity.home.private_key}" > /mnt/data/zerotier-one/identity.secret && \
+        docker run -d --name=zerotier-one --device /dev/net/tun -v /mnt/data/zerotier-one:/var/lib/zerotier-one --net=host --cap-add=NET_ADMIN --cap-add=SYS_ADMIN bltavares/zerotier && \
+        docker exec zerotier-one zerotier-cli join ${zerotier_network.jtcressy_net.id}
+        EOT
     }
-  }
-  sensitive = true
-}
-
-output "zerotier_setup_command" {
-  value = {
-    home = <<-EOT
-    mkdir -p /mnt/data/zerotier-one && \
-    echo "${zerotier_network.jtcressy_net.id}=eth12" > /mnt/data/zerotier-one/devicemap && \
-    echo "${zerotier_identity.home.public_key}" > /mnt/data/zerotier-one/identity.public && \
-    echo "${zerotier_identity.home.private_key}" > /mnt/data/zerotier-one/identity.secret && \
-    docker run -d --name=zerotier-one --device /dev/net/tun -v /mnt/data/zerotier-one:/var/lib/zerotier-one --net=host --cap-add=NET_ADMIN --cap-add=SYS_ADMIN bltavares/zerotier && \
-    docker exec zerotier-one zerotier-cli join ${zerotier_network.jtcressy_net.id}
-    EOT
   }
   sensitive = true
 }
