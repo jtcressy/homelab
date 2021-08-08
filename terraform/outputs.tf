863cfddbb5026e302d0f9e4c088d2681d6616084
Joel Cressy <joel@jtcressy.net>
2021-08-08 06:22:29 +0000

    add output for zt docker command

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -22,3 +22,10 @@ output "zerotier_identities" {
   }
   sensitive = true
 }
+
+output "zerotier_docker_command" {
+  value = {
+    home = "docker run -d --name=zerotier-one --privileged --cap-add NET_ADMIN --device /dev/net/tun -e ZEROTIER_IDENTITY_PUBLIC='${zerotier_identity.home.public_key}' -e ZEROTIER_IDENTITY_SECRET='${zerotier_identity.home.private_key}' zerotier/zerotier:latest ${zerotier_network.jtcressy_net.id}"
+  }
+  sensitive = true
+}
