60aaefc683e7f2ad7e4852496ecd3667d2951bb1
Joel Cressy <joel@jtcressy.net>
2021-09-16 02:49:31 +0000

    thiccer gke nodes

--- terraform-layers/cloud/clusters.tf
+++ terraform-layers/cloud/clusters.tf
@@ -91,7 +91,7 @@ resource "google_container_node_pool" "primary_core-system" {
   node_locations = [each.key]
   node_config {
     preemptible  = true
-    machine_type = "e2-medium"
+    machine_type = "n2d-standard-2"
     disk_size_gb = 20
     workload_metadata_config {
       node_metadata = "GKE_METADATA_SERVER"
