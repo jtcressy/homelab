cbf041e40478a6da094f443023191cc66bd34942
Joel Cressy <joel@jtcressy.net>
2022-01-26 22:45:15 +0000

    first attempt at rpi image customization pipeline

---
+++ hack/custom-tools/rpi-image-cloud-init/go.mod
@@ -0,0 +1,9 @@
+module home.jtcressy.net/hack/rpi-image-cloud-init
+
+go 1.13
+
+require (
+	github.com/diskfs/go-diskfs v1.2.0
+	github.com/frankban/quicktest v1.14.0 // indirect
+	github.com/hashicorp/go-getter v1.5.11
+)
---
+++ hack/custom-tools/rpi-image-cloud-init/main.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/cheggaaa/pb"
+	diskfs "github.com/diskfs/go-diskfs"
+	"github.com/diskfs/go-diskfs/filesystem"
+	"github.com/hashicorp/go-getter"
+	"io"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+//func CustomizeImage(w http.ResponseWriter, r *http.Request) {
+//	fileName := "ubuntu-rpi.img"
+//	fileDestination := "/tmp/" + fileName
+//	ubuntuImageUrl := "https://cdimage.ubuntu.com/releases/20.04/release/ubuntu-20.04.3-preinstalled-server-arm64+raspi.img.xz"
+//	client := &getter.Client{
+//		Ctx:  context.Background(),
+//		Dst:  fileDestination,
+//		Dir:  false,
+//		Src:  ubuntuImageUrl,
+//		Mode: getter.ClientModeFile,
+//	}
+//	log.Println("Downloading ubuntu image: " + ubuntuImageUrl)
+//	if err := client.Get(); err != nil {
+//		log.Fatal(err)
+//		fmt.Fprintf(w, html.EscapeString(err.Error()))
+//		return
+//	}
+//	disk, err := diskfs.Open(fileDestination)
+//	if err != nil {
+//		log.Fatal(err)
+//		fmt.Fprintf(w, html.EscapeString(err.Error()))
+//		return
+//	}
+//	fs, err := disk.GetFilesystem(1)
+//	if err != nil {
+//		log.Fatal(err)
+//		fmt.Fprintf(w, html.EscapeString(err.Error()))
+//		return
+//	}
+//	f, err := fs.OpenFile("/user-data", os.O_RDWR)
+//	if err != nil {
+//		log.Fatal(err)
+//		fmt.Fprintf(w, html.EscapeString(err.Error()))
+//		return
+//	}
+//
+//	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", fileName))
+//	w.Header().Set("Content-Type", "application/octet-stream")
+//	io.Copy(w, f)
+//}
+
+type CommandOptions struct {
+	ImageFileLocation        string
+	BootConfigFolderLocation string
+	DownloadDefaultImage     bool
+}
+
+var (
+	cmdOptions                      CommandOptions
+	defaultUbuntuImageUrl           = "https://cdimage.ubuntu.com/releases/20.04/release/ubuntu-20.04.3-preinstalled-server-arm64+raspi.img.xz"
+	defaultImageFileLocation        = "/tmp/ubuntu-pi.img"
+	defaultBootConfigFolderLocation = "./boot/"
+	defaultBootFileList             = []string{
+		"cmdline.txt",
+		"meta-data",
+		"network-config",
+		"user-data",
+		"usercfg.txt",
+	}
+)
+
+func main() {
+
+	flag.StringVar(
+		&cmdOptions.ImageFileLocation,
+		"image-file",
+		defaultImageFileLocation,
+		"Absolute path to the ubuntu preinstalled cloud image for raspberry pi (.img)",
+	)
+	flag.StringVar(
+		&cmdOptions.BootConfigFolderLocation,
+		"boot-configs",
+		defaultBootConfigFolderLocation,
+		"Absolute path to the folder containing custom boot partition files",
+	)
+	flag.BoolVar(
+		&cmdOptions.DownloadDefaultImage,
+		"download-default",
+		false,
+		"Set true if you want to download the default ubuntu image first",
+	)
+	flag.Parse()
+
+	if cmdOptions.DownloadDefaultImage {
+		client := &getter.Client{
+			Ctx:              context.Background(),
+			Dst:              cmdOptions.ImageFileLocation,
+			Dir:              false,
+			Src:              defaultUbuntuImageUrl,
+			Mode:             getter.ClientModeFile,
+			ProgressListener: &ProgressBar{},
+		}
+		if err := client.Get(); err != nil {
+			panic(err)
+		}
+	}
+
+	disk, err := diskfs.Open(cmdOptions.ImageFileLocation)
+	if err != nil {
+		panic(err)
+	}
+	fs, err := disk.GetFilesystem(1)
+	if err != nil {
+		panic(err)
+	}
+	for _, fileName := range defaultBootFileList {
+		if err := OpenAndCopy(fileName, cmdOptions.BootConfigFolderLocation, fs); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func OpenAndCopy(fileName, bootConfigFolderLocation string, fs filesystem.FileSystem) error {
+	src, err := os.OpenFile(filepath.Join(bootConfigFolderLocation, fileName), os.O_RDONLY, 0777)
+	if err != nil {
+		return err
+	}
+	dest, err := fs.OpenFile(fileName, os.O_RDWR)
+	if err != nil {
+		return err
+	}
+	written, err := io.Copy(dest, src)
+	if err != nil {
+		return fmt.Errorf("copy interrupted for %s at %d, %s", fileName, written, err)
+	}
+	return nil
+}
+
+// ProgressBar wraps a github.com/cheggaaa/pb.Pool
+// in order to display download progress for one or multiple
+// downloads.
+//
+// If two different instance of ProgressBar try to
+// display a progress only one will be displayed.
+// It is therefore recommended to use DefaultProgressBar
+type ProgressBar struct {
+	// lock everything below
+	lock sync.Mutex
+
+	pool *pb.Pool
+
+	pbs int
+}
+
+func ProgressBarConfig(bar *pb.ProgressBar, prefix string) {
+	bar.SetUnits(pb.U_BYTES)
+	bar.Prefix(prefix)
+}
+
+// TrackProgress instantiates a new progress bar that will
+// display the progress of stream until closed.
+// total can be 0.
+func (cpb *ProgressBar) TrackProgress(src string, currentSize, totalSize int64, stream io.ReadCloser) io.ReadCloser {
+	cpb.lock.Lock()
+	defer cpb.lock.Unlock()
+
+	newPb := pb.New64(totalSize)
+	newPb.Set64(currentSize)
+	ProgressBarConfig(newPb, filepath.Base(src))
+	if cpb.pool == nil {
+		cpb.pool = pb.NewPool()
+		cpb.pool.Start()
+	}
+	cpb.pool.Add(newPb)
+	reader := newPb.NewProxyReader(stream)
+
+	cpb.pbs++
+	return &readCloser{
+		Reader: reader,
+		close: func() error {
+			cpb.lock.Lock()
+			defer cpb.lock.Unlock()
+
+			newPb.Finish()
+			cpb.pbs--
+			if cpb.pbs <= 0 {
+				cpb.pool.Stop()
+				cpb.pool = nil
+			}
+			return nil
+		},
+	}
+}
+
+type readCloser struct {
+	io.Reader
+	close func() error
+}
+
+func (c *readCloser) Close() error { return c.close() }
