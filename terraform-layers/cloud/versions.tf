43a0a9c87b998738a384546e7798655631d46c42
Joel Cressy <joel@jtcressy.net>
2022-02-19 19:28:12 +0000

    update terraform cloud layer

--- terraform-layers/cloud/versions.tf
+++ terraform-layers/cloud/versions.tf
@@ -1,10 +1,12 @@
 terraform {
   required_providers {
     google-beta = {
-      source = "hashicorp/google-beta"
+      source  = "hashicorp/google-beta"
+      version = "~>4.11"
     }
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
+      version = "~>4.11"
     }
     random = {
       source = "hashicorp/random"
