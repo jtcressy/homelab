f5bba12ca65bd79891f69bcefa93f1e75dbd9d94
Joel Cressy <joel@jtcressy.net>
2022-02-28 21:16:02 +0000

    create workaround for tailscale default gateway bug tailscale/tailscale#4038

--- onprem-bootstrap/playbooks/files/udmp/onboot/11-onboot-tailscale.sh
+++ onprem-bootstrap/playbooks/files/udmp/onboot/11-onboot-tailscale.sh
@@ -3,6 +3,12 @@ CONTAINER=tailscaled
 IMAGE=ghcr.io/tailscale/tailscale:v1.22.0
 # Starts a Tailscale container that is deleted after it is stopped.
 # All configs stored in /mnt/data/tailscale
+
+# Temporary workaround: copy default route to main table so that tailscale properly discovers it
+if ![ "$(ip -4 route show)" ~= "default via" ] && [ "$(ip -4 route show table 201)" ~= "default via" ]; then
+  ip route add $(ip -4 route show table 201 | grep "default via" | sed -r 's/table 201 /table 0 /g' | sed -r 's/proto dhcp//g')
+fi
+
 if podman container exists ${CONTAINER} && [ "$(podman inspect ${CONTAINER} | jq -r '.[].ImageName')" != "$IMAGE" ]; then
   podman stop ${CONTAINER}
   podman rm -f ${CONTAINER}
